00e9eb64e1b12c01c9069ba7ae0ebc5ce40514a8
PawelHeleniak <pawel.heleniak01@gmail.com>
2023-02-26 17:44:02 +0100

    Nav fix

--- frontend/src/components/Bookmark.jsx
+++ frontend/src/components/Bookmark.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export function Bookmark({ data, handleActive, link }) {
+export function Bookmark({ data, active, handleActive }) {
   return (
-    <div className={data.active ? "bookmark active" : "bookmark"}>
-      <Link to={data.link} onClick={handleActive}>
+    <div className={active === data.name ? "bookmark active" : "bookmark"}>
+      <Link to={data.link} onClick={(e) => handleActive(data.name)}>
         {data.name}
       </Link>
     </div>
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,44 +1,47 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Bookmark } from "./Bookmark";
 import { Link } from "react-router-dom";
+import user from "../assets/icon/user.png";
 
 export function Nav() {
   const bookmarks = [
     {
       name: "Dashboard",
       link: "/dashboard",
-      active: true,
     },
     {
       name: "Konto",
       link: "/dashboard",
-      active: false,
     },
     {
       name: "Pracownicy",
       link: "/dashboard",
-      active: false,
     },
   ];
   const [open, setOpen] = useState(false);
+  const [active, setActive] = useState("Dashboard");
 
-  const handleActive = () => {
-    console.log("s");
+  //change active bookmark
+  const handleActive = (e) => {
+    setActive(e);
   };
 
   const bookmark = bookmarks.map((val) => (
-    <Bookmark data={val} handleActive={handleActive} />
+    <Bookmark data={val} active={active} handleActive={handleActive} />
   ));
 
+  //active nav
   let menuRef = useRef();
   useEffect(() => {
     let handler = (e) => {
-      console.log(menuRef.current);
-      if (menuRef.current.contains(e.target)) {
+      if (!menuRef.current.contains(e.target)) {
         setOpen(false);
       }
     };
     document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
   });
 
   return (
@@ -51,13 +54,9 @@ export function Nav() {
       >
         <p>O</p>
       </div>
-      <div className={`menu ${!open ? "active" : ""}`} ref={menuRef}>
-        <div className="title">
-          <div className="user-permissions">
-            <h1>Admin</h1>
-          </div>
-        </div>
+      <div className={`menu ${open ? "active" : ""}`} ref={menuRef}>
         <div className="user-informations">
+          <img src={user} alt="" />
           <h2>Imie Nazwisko</h2>
           <h3>Admin</h3>
         </div>
